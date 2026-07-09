171978dc63d2835932a27b0b881114370312ed34
Yuki Wada <134752847+y-wada-dev@users.noreply.github.com>
2026-07-09 23:02:18 +0900

    leetcode: remove-duplicates-from-sorted-array easy two-pointers

---
+++ leetcode/easy/026_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow = 0
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+        return slow + 1
