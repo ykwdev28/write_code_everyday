43aac5e8166f90b5aff8867ba41a254505d60641
Yuki Wada <134752847+y-wada-dev@users.noreply.github.com>
2026-08-06 21:39:35 +0900

    leetcode: path-sum easy recursion

---
+++ leetcode/easy/112_path_sum.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: bool
+        """
+        if root is None:
+            return False
+        if root.left is None and root.right is None:
+            return targetSum == root.val
+        return (self.hasPathSum(root.left, targetSum - root.val)
+        or self.hasPathSum(root.right, targetSum - root.val))
